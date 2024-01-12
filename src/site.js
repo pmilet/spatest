4ff63c2615d80a27cf4cd076322758e9e87fa528
Pierre Milet LLobet <pierreml@microsoft.com>
2024-01-12 13:52:17 +0100

    ok

--- src/site.js
+++ src/site.js
@@ -1,8 +1,7 @@
 // The base API Management URI
-const baseUri = "https://duendebffpoc.azure-api.net";
+const apimbaseUri = "https://duendebffpoc.azure-api.net";
 
 // The Azure AD application registration client ID
-//const clientId = "7edbc56c-0081-44ef-93a4-fa33d982566e";
 //const clientId = "interactive.confidential";
 const clientId = "web";
 
@@ -20,19 +19,14 @@ const scope = "api1"
 // Redirects the user to the login endpoint with the appropriate parameters to begin the authentication flow
 const login = () => {
     window.location.href =
-        `${authEndpoint}?response_type=code&redirect_uri=${baseUri}/auth/callback&client_id=${clientId}&scope=${scope}`;
+        `${authEndpoint}?response_type=code&redirect_uri=${apimbaseUri}/auth/callback&client_id=${clientId}&scope=${scope}`;
 };
-
-//const login = () => {
-//    window.location.href =
-//        `${authEndpoint}?response_type=code&redirect_uri=https://identityserverribtest.azurewebsites.net/signin-oidc&client_id=${clientId}&scope=${scope}`;
-//};
 
 // Logs the user out of the application by redirecting to the logout endpoint of Azure AD which will in turn call the logout endpoint of the application to remove the cookie
 // This allows the user to be logged out of Azure AD and the single-page application itself by deleting the cookie
 // If you do not want to log the user out of Azure AD, you can remove the redirect to the logout endpoint of Azure AD and just call the logout endpoint of the application
 const logout = () => {
-    window.location.href = `${logoutEndpoint}?post_logout_redirect_uri=${baseUri}/auth/logout`;
+    window.location.href = `${logoutEndpoint}?post_logout_redirect_uri=${apimbaseUri}/auth/logout`;
 };
 
 // Calls the graph endpoint and displays the result
@@ -41,7 +35,7 @@ const callApi = async () => {
     document.getElementById("result").innerText = "Loading...";
 
     // Call the Graph API endpoint
-    await fetch(`${baseUri}/graph/me`, {
+    await fetch(`https://identityserverribtest.azurewebsites.net/identity`, {
         credentials: "include",
     })
         .then(async (response) => {
