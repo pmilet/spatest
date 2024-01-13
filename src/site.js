5c0878df2cdbc939abae6e5ecac9086ed848a308
Pierre Milet LLobet <pierreml@microsoft.com>
2024-01-13 16:03:51 +0100

    ok

--- src/site.js
+++ src/site.js
@@ -1,54 +1,46 @@
-// The base API Management URI
-const apimbaseUri = "https://duendebffpoc.azure-api.net";
+const config = {
+    apimBaseUri: "https://duendebffpoc.azure-api.net",
+    clientId: "web",
+    authEndpoint: "https://identityserverribtest.azurewebsites.net/connect/authorize",
+    logoutEndpoint: "https://identityserverribtest.azurewebsites.net/connect/endsession",
+    scope: "api1"
+};
 
-// The Azure AD application registration client ID
-//const clientId = "interactive.confidential";
-const clientId = "web";
+const buildLoginUrl = () => {
+    return `${config.authEndpoint}?response_type=code&redirect_uri=${config.apimBaseUri}/auth/callback&client_id=${config.clientId}&scope=${config.scope}`;
+};
 
-//const authEndpoint = "https://demo.duendesoftware.com/connect/authorize";
-//const logoutEndpoint = "https://demo.duendesoftware.com/connect/endsession";
+const buildLogoutUrl = () => {
+    return `${config.logoutEndpoint}?post_logout_redirect_uri=${config.apimBaseUri}/auth/logout`;
+};
 
-const authEndpoint = "https://identityserverribtest.azurewebsites.net/connect/authorize";
-const logoutEndpoint = "https://identityserverribtest.azurewebsites.net/connect/endsession";
-
-// The scope for the access token request to call the Microsoft Graph API
-// If a refresh token is also required for the application, add "offline_access" to the scope
-// e.g. const scope = "https://graph.microsoft.com/.default offline_access"
-const scope = "api1"
-
-// Redirects the user to the login endpoint with the appropriate parameters to begin the authentication flow
 const login = () => {
-    window.location.href =
-        `${authEndpoint}?response_type=code&redirect_uri=${apimbaseUri}/auth/callback&client_id=${clientId}&scope=${scope}`;
+    window.location.href = buildLoginUrl();
 };
 
-// Logs the user out of the application by redirecting to the logout endpoint of Azure AD which will in turn call the logout endpoint of the application to remove the cookie
-// This allows the user to be logged out of Azure AD and the single-page application itself by deleting the cookie
-// If you do not want to log the user out of Azure AD, you can remove the redirect to the logout endpoint of Azure AD and just call the logout endpoint of the application
 const logout = () => {
-    window.location.href = `${logoutEndpoint}?post_logout_redirect_uri=${apimbaseUri}/auth/logout`;
+    window.location.href = buildLogoutUrl();
 };
 
-// Calls the graph endpoint and displays the result
 const callApi = async () => {
-    // Display loading message
-    document.getElementById("result").innerText = "Loading...";
+    const resultElement = document.getElementById("result");
+    resultElement.innerText = "Loading...";
 
-    // Call the Graph API endpoint
-    await fetch(`https://api6543.azurewebsites.net/identity`, {
-        credentials: "include",
-    })
-        .then(async (response) => {
-            if (response.status === 401) {
-                document.getElementById("result").innerText = "User is not authenticated.";
-            } else {
-                document.getElementById("result").innerText = JSON.stringify(await response.json(), null, 4);
-            }
-        })
-        .catch((error) => {
-            document.getElementById("result").innerText = error;
+    try {
+        const response = await fetch(`https://api6543.azurewebsites.net/identity`, {
+            method: "GET",
+            credentials: "include"
         });
+
+        if (response.status === 401) {
+            resultElement.innerText = "User is not authenticated.";
+        } else {
+            const data = await response.json();
+            resultElement.innerText = JSON.stringify(data, null, 4);
+        }
+    } catch (error) {
+        resultElement.innerText = `Error: ${error.message}`;
+    }
 };
 
-// Exports the functions to be used in the HTML
 export { login, logout, callApi };
